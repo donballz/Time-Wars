9c8f22162b3fe35695391456fedf0885f2dc4baf
Donald Armstead <donald.armstead@gmail.com>
2017-02-18 00:31:34 -0500

    feat: add support for reading Points from mysql

--- sql_store.rb
+++ sql_store.rb
@@ -1,6 +1,7 @@
 require 'mysql'
 require 'yaml'
 require_relative 'CONSTANTS.rb'
+require_relative 'Point4D.rb'
 require_relative 'hunt_dwho.rb'
 
 # SQL Code to create the template
@@ -22,7 +23,7 @@ def to_sql(pt_set, table)
 		con.query("CREATE TABLE #{table} AS SELECT * FROM TEMPLATE")
 		pt_set.each do |pt|
 			con.query("INSERT INTO #{table}
-						SELECT #{pt.X}, #{pt.Y}, #{pt.Z}, #{pt.T} FROM DUAL")
+						SELECT #{pt.x}, #{pt.y}, #{pt.z}, #{pt.t} FROM DUAL")
 		end
 		
 	rescue Mysql::Error => e
@@ -34,9 +35,33 @@ def to_sql(pt_set, table)
 	end
 end
 
+def fr_sql(table)
+	# reads data from sql table and outputs list of 4D points
+	output = []
+	begin
+		con = Mysql.new SRVR, USER, PSWD
+		con.query("USE #{SCMA}")
+		rows = con.query("SELECT * FROM #{table}")
+		rows.each_hash do |row|
+			output.push(Point4D.new(row['X'], row['Y'], row['Z'], row['T']))
+		end
+		
+	rescue Mysql::Error => e
+		puts e.errno
+		puts e.error
+
+	ensure
+		con.close if con
+	end
+	return output
+end
+
 def Testing()
-	misses = planet_data(PLANET, 'X')
-	to_sql(misses, PLANET)
+	#misses = planet_data(PLANET, 'X')
+	#to_sql(misses, PLANET)
+	misses = fr_sql(PLANET)
+	misses.each { |m| puts m }
+	puts misses.length
 end
 
 now = Time.now
